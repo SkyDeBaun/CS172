3c72cd4229bf447e09794b7ced5b98df6aac19f2
SkyDeBaun <sdeba001@ucr.edu>
2021-04-21 20:46:38 -0700

    added 'if __main__'

--- assignment01/parsing.py
+++ assignment01/parsing.py
@@ -110,69 +110,72 @@ def get_doc_id(doc, index = docIndex):
 #--------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------- MAIN
 #--------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
 
 
-'''
-with zipfile.ZipFile("ap89_collection_small.zip", 'r') as zip_ref:
-    zip_ref.extractall()
-'''
 
 
-#create stopword list--------------------------
+    '''
+    with zipfile.ZipFile("ap89_collection_small.zip", 'r') as zip_ref:
+        zip_ref.extractall()
+    '''
 
-create_stopword_set("stopwords.txt")
+
+    #create stopword list--------------------------
+
+    create_stopword_set("stopwords.txt")
 
 
-# Retrieve the names of all files to be indexed in folder ./ap89_collection_small of the current directory
-for dir_path, dir_names, file_names in os.walk("ap89_collection_small"):
-    allfiles = [os.path.join(dir_path, filename).replace("\\", "/") for filename in file_names if (filename != "readme" and filename != ".DS_Store")]
-    
-    
-for idx, file in enumerate(allfiles): #adding index to loop using enumerate function (to provide index for the doc dictionary)
-    with open(file, 'r', encoding='ISO-8859-1') as f:
-        filedata = f.read()
-        result = re.findall(doc_regex, filedata)  # Match the <DOC> tags and fetch documents
+    # Retrieve the names of all files to be indexed in folder ./ap89_collection_small of the current directory
+    for dir_path, dir_names, file_names in os.walk("ap89_collection_small"):
+        allfiles = [os.path.join(dir_path, filename).replace("\\", "/") for filename in file_names if (filename != "readme" and filename != ".DS_Store")]
         
         
-
-        for document in result[0:]:
-            # Retrieve contents of DOCNO tag
-            docno = re.findall(docno_regex, document)[0].replace("<DOCNO>", "").replace("</DOCNO>", "").strip()
-            
-            #create index of docno to doc path/name
-            #print(str(idx) + ".) docno: " + docno)
-            docIndex.__setitem__(docno, file) #document index -> key: docno, value: document path/name
-
-
-            # Retrieve contents of TEXT tag
-            text = "".join(re.findall(text_regex, document))\
-                      .replace("<TEXT>", "").replace("</TEXT>", "")\
-                      .replace("\n", " ")
+    for idx, file in enumerate(allfiles): #adding index to loop using enumerate function (to provide index for the doc dictionary)
+        with open(file, 'r', encoding='ISO-8859-1') as f:
+            filedata = f.read()
+            result = re.findall(doc_regex, filedata)  # Match the <DOC> tags and fetch documents
             
             
 
-            # step 1 - lower-case words, remove punctuation, remove stop-words, etc. 
-            # step 2 - create tokens 
-            # step 3 - build index
-
-sleep(2)
-
-
-print("SHOW ME:")
-mylist = ["hello", "world", "how", "are", "yoooooooo", "hello", "world"]
-
-for i in mylist:
-    mykey = add_token(i)
-    print(mykey)
+            for document in result[0:]:
+                # Retrieve contents of DOCNO tag
+                docno = re.findall(docno_regex, document)[0].replace("<DOCNO>", "").replace("</DOCNO>", "").strip()
+                
+                #create index of docno to doc path/name
+                #print(str(idx) + ".) docno: " + docno)
+                docIndex.__setitem__(docno, file) #document index -> key: docno, value: document path/name
 
 
-sleep(1)    
-print(termIndex)
+                # Retrieve contents of TEXT tag
+                text = "".join(re.findall(text_regex, document))\
+                        .replace("<TEXT>", "").replace("</TEXT>", "")\
+                        .replace("\n", " ")
+                
+                
+
+                # step 1 - lower-case words, remove punctuation, remove stop-words, etc. 
+                # step 2 - create tokens 
+                # step 3 - build index
+
+    sleep(2)
+
+
+    print("SHOW ME:")
+    mylist = ["hello", "world", "how", "are", "yoooooooo", "hello", "world"]
+
+    for i in mylist:
+        mykey = add_token(i)
+        print(mykey)
+
+
+    sleep(1)    
+    print(termIndex)
 
 
 
-print("Token ID: " + str(get_token_id("are")))
+    print("Token ID: " + str(get_token_id("are")))
 
 
-sleep(1)
-print(stopWordSet)
+    sleep(1)
+    print(stopWordSet)

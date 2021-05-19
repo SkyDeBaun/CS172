e449c14eddf927c158180bb36a261fe71fa29fa9
SkyDeBaun <sdeba001@ucr.edu>
2021-05-19 00:05:07 -0700

    example for separating query num and query

--- assignment02/read_test.py
+++ assignment02/read_test.py
@@ -1,21 +1,27 @@
 #feature extraction --> https://scikit-learn.org/stable/modules/feature_extraction.html
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-
-
+import re
 
 #read the queries from text file-----------------------
 with open ('query_list.txt', 'r') as file:
   query_list = file.readlines() #unprocesed list of queries
 
-queries = [text[5:].strip().lower() for text in query_list] #list comprehension
+#queries = [text[5:].strip().lower() for text in query_list] #list comprehension
 
-#print(queries)
+for text in query_list:
+    q_num = text[:4].strip()
+    num = re.sub('[.]', '', q_num)
+
+    query = text[5:].strip().lower()
+    print(num + ": "  + query)
+    
+    #print(q)
 
 
 
-tfidf = TfidfVectorizer()
 
-vec = tfidf.fit_transform(queries)
-print(vec)
+#tfidf = TfidfVectorizer()
+
+#vec = tfidf.fit_transform(queries)
+#print(type(vec))
+#print(vec)
